dbf8eba9f9c1fdb9f139e38fa866246b2afb635d
Tatyana <mitrofanowa.t2013@gmail.com>
2023-07-13 21:09:10 +0300

    решение задачи № 64

---
+++ Задача 64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int number = 8;
+int n = 1;
+Perechislenie(number, n);
+
+void Perechislenie(int z, int n)
+{
+    if (z < n)
+    {
+        return;
+    }
+    else
+    {
+       Perechislenie(z, n+1); 
+        Console.Write(n + "  ");
+        
+
+    }    
+}
